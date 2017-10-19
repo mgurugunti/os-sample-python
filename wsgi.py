a896d979264b2bfacee51cb23d23d900335da4b5
Mahesh Gurugunti <mahesh.gurugunti@tdameritrade.com>
2017-10-18 20:21:44 -0400

    Another change

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello OpenShift Mahesh Cloud Test World!"
+    return "Hello OpenShift 3.6 Mahesh Cloud Test World!"
 
 if __name__ == "__main__":
     application.run()
