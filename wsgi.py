da245464116d1cbbc5adce1028ec6fe7ffad7ee2
mgurugunti <mgurugunti@users.noreply.github.com>
2017-11-30 14:28:48 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Mahesh's OpenShift World!"
+    return "Hello Andy's OpenShift World!"
 
 if __name__ == "__main__":
     application.run()
